b97ed00d1afb12517b18d08e8466a85563e65568
Phan Quoc Thai <pq_thai@yahoo.com.vn>
2018-12-29 12:41:48 +0700

    Hi

--- UnitTests/XOProject.Api.Tests/AnalyticsControllerTest.cs
+++ UnitTests/XOProject.Api.Tests/AnalyticsControllerTest.cs
@@ -222,6 +222,13 @@ namespace XOProject.Api.Tests
                     Symbol = "REL",
                     Rate = 950.0M,
                     TimeStamp = new DateTime(2018, 12, 4, 6, 0, 0)
+                },
+                  new HourlyShareRate
+                {
+                    Id = 15,
+                    Symbol = "REL",
+                    Rate = 350.0M,
+                    TimeStamp = new DateTime(2018, 12, 4, 8, 0, 0)
                 }
             };
             //_analyticsServiceMock.Object._shareControlRepository = _shareRepositoryMock.Object;
